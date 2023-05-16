5f9a02acfb471b1fa31f227372e69253de0e3dc2
hanalau5 <joiashanna16@gmail.com>
2023-05-15 21:51:13 -0300

    update prj Hannabooks rev.9 15/05/2023

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,20 @@
 /* Arquivo das automações do Menu Hamburguer*/
+
+const btnMobile = document.getElementById('btn-mobile');
+// Declarando a variável com o CONST
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault();
+    const nav =  document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fecha Menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label','Abrir Menu');
+        }
+}
+
+btnMobile.addEventListener('click',toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
